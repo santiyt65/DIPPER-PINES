3b22fc26a638e6a7c72ab451f14b8915dd98d21f
L.N. <santiloez6@gmail.com>
2024-09-30 14:05:05 -0300

    DIPPER-PINES-BOT

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ const { name, author } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
  
-say('Dipper\nBot', {
+say('DIPPER\nBot', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
-say(`Project Author:\nWilmer oficial (@santiyt65)`, {
+say(`Project Author:\nWilmer oficial (@Wilsmac)`, {
 font: 'console',
 align: 'center',
 gradient: ['red', 'magenta']})
